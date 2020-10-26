3173a61977b971e1817d2015d656e27be7a92cf2
umairsadiq88 <umairsadiq2010@gmail.com>
2020-10-26 23:23:24 +0500

    css

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import Launch from './components/Launch/Launch'
 import LaunchIndex from './components/Launch/LaunchIndex'
 import LaunchShoe from './components/Launch/LaunchShoe'
 import NotFound from './components/NotFound'
-
+import {Nav} from 'react-bootstrap;'
 
 import './App.css';
 
 
 function App() {
   return <Router>
-       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/Launch">Launch</Link>
+      
+       <nav bg="dark" variant="dark">
+    <Link to="/">Home</Link>
+      <Link to="/Launch">Launch</Link>
+      {/* <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+     */}
       </nav>
 
     <Routes>
@@ -23,6 +28,7 @@ function App() {
         <Route path="/" element={<LaunchIndex />} />
         <Route path=":slug" element={<LaunchShoe />} />
       </Route>
+      
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
