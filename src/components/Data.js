ee9dd711c40a4ee3f3234928c694a18f650e7e0f
Umair Sadiq <59564704+umairsadiq88@users.noreply.github.com>
2021-02-04 01:00:03 +0500

    update

---
+++ src/components/Data.js
@@ -0,0 +1,69 @@
+const Data = {
+  kidShoes: {
+        "air-jordan-3-valor-blue": {
+          name: "VALOUR BLUE",
+          img:
+            "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+        },
+        "jordan-mars-270-london": {
+          name: "JORDAN MARS 270 LONDON",
+          img:
+            "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+        },
+        "Girls Flower Sandals": {
+          name: "GIRLS FLOWER SANDALS",
+          img:
+            "https://cdn.shopify.com/s/files/1/0262/5804/7069/products/1_2c1e17f8-6588-408f-922d-80bdb088e507.jpg?v=1601271700"
+        },
+        "Buckled Girls Pumps": {
+          name: "BUCKLED GIRLS PUMPS",
+          img:
+            "https://cdn.shopify.com/s/files/1/0262/5804/7069/products/IMG_0589copy_06bef632-639a-4de7-85e1-36b802475ba5.jpg?v=1601271350"
+        }
+      },
+     menShoes: {
+        "Buckled Mens Chappals": {
+          name: "BUCKLED MENS CHAPPALS",
+          img:
+            "https://cdn.shopify.com/s/files/1/0262/5804/7069/products/IMG_8575_copy_5dacb855-e227-4e82-b471-733be839a4f1.jpg?v=1601289907"
+        },
+        "Abrex Mens Chappals": {
+          name: "ABREX MENS CHAPPALS",
+          img:
+            "https://cdn.shopify.com/s/files/1/0262/5804/7069/products/1_d4bd157a-fe29-43a6-aa14-e35ef0729d62.jpg?v=1601270414"
+        },
+        "Tassled Mens Loafers": {
+          name: "TASSLED MENS LOAFERS",
+          img:
+            "https://cdn.shopify.com/s/files/1/0262/5804/7069/products/1_08e5720e-d461-4a98-8857-0931be191153.jpg?v=1601289623"
+        },
+        "Mens Performance Shoes": {
+          name: "MENS PERFORMANCE SHOES",
+          img:
+            "https://cdn.shopify.com/s/files/1/0262/5804/7069/products/IMG_5449copy_4c3ffdaa-4242-470a-bfc6-eb941c0df925.jpg?v=1601268986"
+        }
+      },
+      womenShoes: {
+        "Toe Ring Women Chappal": {
+          name: "TOE RING WOMEN CHAPPAL",
+          img:
+            "https://cdn.shopify.com/s/files/1/0262/5804/7069/products/1_dc59d715-cdd0-4a21-9f88-2564b7b39968.jpg?v=1601272427"
+        },
+        "Printed Chappals": {
+          name: "PRINTED CHAPPALS",
+          img:
+            "https://cdn.shopify.com/s/files/1/0262/5804/7069/products/IMG_8692copy_ab11594a-ba8d-4f28-81ca-e503d489ffe9.jpg?v=1601289726"
+        },
+        "Flower Print Women Sneakers": {
+          name: "FLOWER PRINT WOMEN SNEAKERS",
+          img:
+            "https://cdn.shopify.com/s/files/1/0262/5804/7069/products/IMG_6425_6f527e3d-a522-4811-9b6a-37c7d9695676.jpg?v=1601287909"
+        },
+        "Casual Comfy Shoes": {
+          name: "CASUAL COMFY SHOES",
+          img:
+            "https://cdn.shopify.com/s/files/1/0262/5804/7069/products/IMG_6460copy_d8628ecf-ad0a-40df-a29a-113802355348.jpg?v=1601287983"
+        }
+      }
+}
+export default Data
--- src/components/Men/Men.jsx
+++ src/components/Men/Men.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Outlet} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import { useParams } from 'react-router-dom'
 
 const Men = () => {
     return (
@@ -8,6 +10,42 @@ const Men = () => {
             <Outlet />
         </div>
     )
+};
+
+
+const MenIndex = () => {
+    return (
+       <ul>
+           {Object.entries(menShoes).map(([slug, {name, img}]) => (
+               <li key={slug}>
+                   <Link to={`/Men/${slug}`}>
+                       <h2>{name}</h2>
+                       <img src={img} alt={name}/>
+                   </Link>
+               </li>
+           ))}
+      </ul>
+    );
 }
 
-export default Men;
+
+const MenShoe = () => {
+    const { slug } = useParams();
+    const menShoe = menShoes[slug];
+
+    if (!menShoe) {
+        return <h2>Not Found!</h2>
+    }
+
+    const { name, img } = menShoe;
+    return (
+        <div>
+            <h2>{name}</h2>
+            <img src={img} alt={name} />
+        </div>
+    )
+}
+
+
+export default Men; 
+export { MenIndex, MenShoe };
