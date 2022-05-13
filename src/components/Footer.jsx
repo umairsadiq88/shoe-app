f093c6e52d72481d1ad4d9151162be570fb1eda5
Umair Sadiq <59564704+umairsadiq88@users.noreply.github.com>
2022-05-13 19:52:58 +0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,13 @@ import "../index.css"
 
 const Footer = () => {
     return (
+        
         <footer className="footer">
-        <p>Developed by: Umair Sadiq contact now:</p> 
-
-        <p>
-      <a href="mailto:umairsadiq2010@gmail.com">umairsadiq2010@gmail.com </a>
-      </p>
+        <p>Developed by: Umair Sadiq</p>
+      
 
 </footer>
+
     )
 }
 
